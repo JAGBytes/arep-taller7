89792f200f7233b5a264757da1c7688f276f1d50
JAGBytes <jorge.gamboa-s@mail.escuelaing.edu.co>
2025-10-25 14:57:58 -0500

    initial

---
+++ src/test/java/co/edu/eci/taller7/Taller7ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.eci.taller7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
